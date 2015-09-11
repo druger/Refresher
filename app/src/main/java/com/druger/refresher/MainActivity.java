a8041fedd62e13f629eec5ba3a4990eae6ea8845
druger <bayt32@yandex.ru>
2015-09-11 17:44:43 +0300

    Added CoordinatorLayout and Splashscreen. Ability turn off Splashscreen

--- app/src/main/java/com/druger/refresher/MainActivity.java
+++ app/src/main/java/com/druger/refresher/MainActivity.java
@@ -1,5 +1,6 @@
 package com.druger.refresher;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,16 +8,29 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
+
+    PreferenceHelper preferenceHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PreferenceHelper.getInstance().init(getApplicationContext());
+        preferenceHelper = PreferenceHelper.getInstance();
+
+        fragmentManager = getFragmentManager();
+
+        runSplash();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem splashItem = menu.findItem(R.id.action_splash);
+        splashItem.setChecked(preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE));
         return true;
     }
 
@@ -28,10 +42,23 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_splash) {
+            item.setChecked(!item.isChecked());
+            preferenceHelper.putBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE, item.isChecked());
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void runSplash() {
+        if (!preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE)) {
+            SplashFragment splashFragment = new SplashFragment();
+
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, splashFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/druger/refresher/SplashFragment.java
@@ -0,0 +1,52 @@
+package com.druger.refresher;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SplashFragment extends Fragment {
+
+
+    public SplashFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        SplashTask splashTask = new SplashTask();
+        splashTask.execute();
+
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    class SplashTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            getActivity().getFragmentManager().popBackStack();
+
+            return null;
+        }
+    }
+
+
+}
