14eae34d151c03ee70c95998bf79901e5af3323d
druger <bayt32@yandex.ru>
2015-09-25 17:34:54 +0300

    Added removal tasks. Added SnackBar which cancels deleting tasks.

--- app/src/main/java/com/druger/refresher/database/DBHelper.java
+++ app/src/main/java/com/druger/refresher/database/DBHelper.java
@@ -34,7 +34,8 @@ public class DBHelper extends SQLiteOpenHelper {
             + TASK_STATUS_COLUMN + " INTEGER, "
             + TASK_TIME_STAMP_COLUMN + " LONG);";
 
-    public static final String SELECTION_STATUS = DBHelper.TASK_STATUS_COLUMN + " = ?";
+    public static final String SELECTION_STATUS = TASK_STATUS_COLUMN + " = ?";
+    public static final String SELECTION_TIME_STAMP = TASK_TIME_STAMP_COLUMN + " = ?";
 
     private DBQueryManager queryManager;
     private DBUpdateManager updateManager;
@@ -75,5 +76,10 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public DBUpdateManager update() {
         return updateManager;
+    }
+
+    public void removeTask(long timeStamp){
+        getWritableDatabase().delete(TASKS_TABLE, SELECTION_TIME_STAMP,
+                new String[]{Long.toString(timeStamp)});
     }
 }
