ba8921faaf258c2868bc9b82c1ff6799a94d3917
druger <bayt32@yandex.ru>
2017-11-28 01:54:41 +0300

    DoneTaskAdapter replaced by kotlin

---
+++ app/src/main/java/com/druger/refresher/adapters/DoneTaskAdapter.kt
@@ -0,0 +1,138 @@
+package com.druger.refresher.adapters
+
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Handler
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.druger.refresher.R
+import com.druger.refresher.fragments.DoneTaskFragment
+import com.druger.refresher.models.ModelTask
+import com.druger.refresher.utils.DateHelper
+import de.hdodenhof.circleimageview.CircleImageView
+
+/**
+* Created by druger on 19.09.2015.
+*/
+class DoneTaskAdapter(taskFragment: DoneTaskFragment) : TaskAdapter(taskFragment) {
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        val view: View = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.model_task, viewGroup, false)
+        val title: TextView = view.findViewById(R.id.tvTaskTitle)
+        val date: TextView = view.findViewById(R.id.tvTaskDate)
+        val priority: CircleImageView = view.findViewById(R.id.cvTaskPriority)
+
+        return TaskViewHolder(view, title, date, priority)
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+
+        val item = items[position]
+
+        if (item.isTask) {
+            viewHolder.itemView.isEnabled = true
+            val task = item as ModelTask
+            val taskViewHolder = viewHolder as TaskViewHolder
+
+            val itemView = taskViewHolder.itemView
+
+            taskViewHolder.title.text = task.title
+            if (task.date != 0L) {
+                taskViewHolder.date.text = DateHelper.getFullDate(task.date)
+            } else {
+                taskViewHolder.date.text = null
+            }
+
+            itemView.visibility = View.VISIBLE
+            taskViewHolder.priority.isEnabled = true
+
+            taskViewHolder.title.setTextColor(ContextCompat.getColor(taskFragment.getActivity(), R.color.primary_text_light))
+            taskViewHolder.date.setTextColor(ContextCompat.getColor(taskFragment.getActivity(), R.color.secondary_text_light))
+            taskViewHolder.priority.setColorFilter(ContextCompat.getColor(taskFragment.getActivity(), task.priorityColor))
+            taskViewHolder.priority.setImageResource(R.drawable.ic_check_circle_white_48dp)
+
+            itemView.setOnLongClickListener({
+                // for delay ripple animation
+                val handler = Handler()
+                handler.postDelayed({
+                    getTaskFragment().removeTaskDialog(
+                            taskViewHolder.layoutPosition)
+                }, 1000)
+            })
+
+            taskViewHolder.priority.setOnClickListener({
+                taskViewHolder.priority.isEnabled = false
+                task.status = ModelTask.STATUS_CURRENT
+                getTaskFragment().activity.realmHelper.updateTask(task)
+
+                taskViewHolder.title.setTextColor(ContextCompat.getColor(taskFragment.getActivity(), R.color.primary_text_light))
+                taskViewHolder.date.setTextColor(ContextCompat.getColor(taskFragment.getActivity(), R.color.secondary_text_light))
+                taskViewHolder.priority.setColorFilter(ContextCompat.getColor(taskFragment.getActivity(), task.priorityColor))
+
+                val flipIn: ObjectAnimator = ObjectAnimator.ofFloat(taskViewHolder.priority, "rotationY", 180f, 0f)
+                taskViewHolder.priority.setImageResource(R.drawable.ic_check_circle_white_48dp)
+
+                flipIn.addListener(object : Animator.AnimatorListener {
+
+                    override fun onAnimationStart(animation: Animator) {
+
+                    }
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        if (task.status != ModelTask.STATUS_DONE) {
+
+                            val translationX: ObjectAnimator = ObjectAnimator.ofFloat(itemView,
+                                    "translationX", 0f, -itemView.width.toFloat())
+
+                            val translationXBack: ObjectAnimator = ObjectAnimator.ofFloat(itemView,
+                                    "translationX", -itemView.width.toFloat(), 0f)
+
+                            translationX.addListener(object : Animator.AnimatorListener {
+
+                                override fun onAnimationStart(animation: Animator) {
+
+                                }
+
+                                override fun onAnimationEnd(animation: Animator) {
+                                    itemView.visibility = View.GONE
+                                    getTaskFragment().moveTask(task)
+                                    removeItem(taskViewHolder.layoutPosition)
+
+                                }
+
+                                override fun onAnimationCancel(animation: Animator) {
+
+                                }
+
+                                override fun onAnimationRepeat(animation: Animator) {
+
+                                }
+                            })
+
+                            val translationSet = AnimatorSet()
+                            translationSet.play(translationX).before(translationXBack)
+                            translationSet.start()
+                        }
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+
+                    }
+
+                    override fun onAnimationRepeat(animation: Animator) {
+
+                    }
+                })
+                flipIn.start()
+            })
+        }
+    }
+}
