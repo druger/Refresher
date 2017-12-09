83d23815e750d2d71afad36025ebc08871dd9d58
druger <bayt32@yandex.ru>
2017-12-10 02:03:46 +0300

    RealmHelper replaced by kotlin

---
+++ app/src/main/java/com/druger/refresher/database/RealmHelper.kt
@@ -0,0 +1,90 @@
+package com.druger.refresher.database
+
+import android.content.Context
+
+import com.druger.refresher.models.ModelTask
+import com.druger.refresher.models.ModelTask.*
+
+import io.realm.Realm
+import io.realm.RealmResults
+
+
+/**
+* Created by druger on 21.09.2015.
+*/
+
+class RealmHelper(context: Context) {
+
+    companion object {
+        const val SCHEMA_VERSION = 1
+        const val REALM_NAME = "refresher.realm"
+
+        const private val TITLE_COLUMN = "title"
+        const private val DATE_COLUMN = "date"
+        const private val STATUS_COLUMN = "status"
+        const private val STAMP_COLUMN = "timeStamp"
+    }
+
+    private var realm: Realm = Realm.getDefaultInstance()
+
+    fun saveTask(task: ModelTask) {
+        realm.beginTransaction()
+        realm.insert(task)
+        realm.commitTransaction()
+    }
+
+    fun getTaskByTimestamp(timestamp: Long): ModelTask? {
+        return realm.where(ModelTask::class.java)
+                .equalTo(STAMP_COLUMN, timestamp)
+                .findFirst()
+    }
+
+    fun getTasksByAnyStatus(): List<ModelTask> {
+        val results: RealmResults<ModelTask> = realm.where(ModelTask::class.java)
+                .equalTo(STATUS_COLUMN, STATUS_CURRENT)
+                .or()
+                .equalTo(STATUS_COLUMN, STATUS_OVERDUE)
+                .findAllSorted(DATE_COLUMN)
+        return realm.copyFromRealm(results)
+    }
+
+    fun getTasksByDoneStatus(): List<ModelTask> {
+        val results: RealmResults<ModelTask> = realm.where(ModelTask::class.java)
+                .equalTo(STATUS_COLUMN, STATUS_DONE)
+                .findAllSorted(DATE_COLUMN)
+        return realm.copyFromRealm(results)
+    }
+
+    fun getTasksByTitleAndAnyStatus(title: String): List<ModelTask> {
+        val results: RealmResults<ModelTask> = realm.where(ModelTask::class.java)
+                .like(TITLE_COLUMN, "*$title*")
+                .equalTo(STATUS_COLUMN, STATUS_CURRENT)
+                .or()
+                .equalTo(STATUS_COLUMN, STATUS_OVERDUE)
+                .findAllSorted(DATE_COLUMN)
+        return realm.copyFromRealm(results)
+    }
+
+    fun getTasksByTitleAndDoneStatus(title: String): List<ModelTask> {
+        val results: RealmResults<ModelTask> = realm.where(ModelTask::class.java)
+                .like(TITLE_COLUMN, "*$title*")
+                .equalTo(STATUS_COLUMN, STATUS_DONE)
+                .findAllSorted(DATE_COLUMN)
+        return realm.copyFromRealm(results)
+    }
+
+    fun updateTask(modelTask: ModelTask) {
+        realm.beginTransaction()
+        realm.copyToRealmOrUpdate(modelTask)
+        realm.commitTransaction()
+    }
+
+    fun removeTaskByTimestamp(timeStamp: Long) {
+        realm.beginTransaction()
+        val results = realm.where(ModelTask::class.java)
+                .equalTo(STAMP_COLUMN, timeStamp)
+                .findAll()
+        results.deleteAllFromRealm()
+        realm.commitTransaction()
+    }
+}
