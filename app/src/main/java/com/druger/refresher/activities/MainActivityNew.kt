53d472cb0722a1a0d9ab8a71eb0ba9faf97e87da
Gromov Evgeny <e.gromov@key-g.com>
2020-02-09 18:49:39 +0300

    setup Navigation components

---
+++ app/src/main/java/com/druger/refresher/activities/MainActivityNew.kt
@@ -0,0 +1,12 @@
+package com.druger.refresher.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.druger.refresher.R
+
+class MainActivityNew : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
