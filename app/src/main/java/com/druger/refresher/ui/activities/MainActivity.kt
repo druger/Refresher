f3b35ab816c9c10446aac216e1ecdc19b4c9aca2
Gromov Evgeny <e.gromov@key-g.com>
2020-03-23 22:48:55 +0300

    setup Navigation component

--- app/src/main/java/com/druger/refresher/ui/activities/MainActivity.kt
+++ app/src/main/java/com/druger/refresher/ui/activities/MainActivity.kt
@@ -5,21 +5,17 @@ import android.os.PersistableBundle
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.druger.refresher.App
 import com.druger.refresher.R
 import com.druger.refresher.adapters.TabAdapter
 import com.druger.refresher.alarms.AlarmHelper
 import com.druger.refresher.database.RealmHelper
+import com.druger.refresher.models.ModelTask
 import com.druger.refresher.ui.dialogs.AddingTaskDialogFragment
 import com.druger.refresher.ui.dialogs.EditTaskDialogFragment
 import com.druger.refresher.ui.fragments.CurrentTaskFragment
 import com.druger.refresher.ui.fragments.DoneTaskFragment
 import com.druger.refresher.ui.fragments.TaskFragment
-import com.druger.refresher.models.ModelTask
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.tabs.TabLayout
-import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(),
@@ -38,16 +34,12 @@ class MainActivity : AppCompatActivity(),
     private lateinit var doneTaskFragment: TaskFragment
 
     private lateinit var searchView: SearchView
-    private lateinit var fab: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
         App.getApplicationComponent().inject(this)
-        setupToolbar()
-        setupViewPager()
         setupUI()
-        setupUX()
     }
 
     override fun onPause() {
@@ -76,42 +68,6 @@ class MainActivity : AppCompatActivity(),
             }
 
         })
-    }
-
-    private fun setupUX() {
-        fab.setOnClickListener {
-            val addingTaskDialogFragment = AddingTaskDialogFragment()
-            addingTaskDialogFragment.show(supportFragmentManager, "AddingTaskDialogFragment")
-        }
-    }
-
-    private fun setupViewPager() {
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.current_task))
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.done_task))
-
-        tabAdapter = TabAdapter(supportFragmentManager, 2)
-
-        viewPager.adapter = tabAdapter
-        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
-
-        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                viewPager.currentItem = tab.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {
-
-            }
-        })
-    }
-
-    private fun setupToolbar() {
-        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
-//        setSupportActionBar(toolbar)
     }
 
     override fun onTaskRestore(task: ModelTask) {
---
+++ app/src/main/java/com/druger/refresher/ui/fragments/MainFragment.kt
@@ -0,0 +1,52 @@
+package com.druger.refresher.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.druger.refresher.R
+import com.druger.refresher.adapters.TabAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+
+    private lateinit var tabAdapter: TabAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewPager()
+        fabAddTask.setOnClickListener { showAddingTaskFragment() }
+    }
+
+    private fun showAddingTaskFragment() {
+        findNavController().navigate(R.id.go_to_addingTaskFragment)
+    }
+
+    private fun setupViewPager() {
+        tabAdapter = TabAdapter(parentFragmentManager, NUMBER_OF_TABS)
+
+        viewPager.adapter = tabAdapter
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+    }
+
+    companion object {
+        private const val NUMBER_OF_TABS = 2
+    }
+}
