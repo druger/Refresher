93b5c7b6ccc278bb6dacf3e1c756a481c87ee0f3
Gromov Evgeny <e.gromov@key-g.com>
2020-04-05 22:42:49 +0300

    wait inserting task

--- app/src/main/java/com/druger/refresher/ui/fragments/AddingTaskFragment.kt
+++ app/src/main/java/com/druger/refresher/ui/fragments/AddingTaskFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.druger.refresher.R
 import com.druger.refresher.models.ModelTaskNew
@@ -12,6 +13,7 @@ import com.druger.refresher.ui.dialogs.TimePickerFragment
 import com.druger.refresher.viewmodel.TaskViewModel
 import kotlinx.android.synthetic.main.fragment_adding_task.*
 import kotlinx.android.synthetic.main.toolbar_adding.*
+import kotlinx.coroutines.launch
 
 class AddingTaskFragment: Fragment(R.layout.fragment_adding_task) {
 
@@ -28,8 +30,10 @@ class AddingTaskFragment: Fragment(R.layout.fragment_adding_task) {
     private fun addTask() {
         val title = etTitle.text.toString().trim()
         if (title.isNotEmpty()) {
-            taskModel.insertTask(ModelTaskNew(title))
-            findNavController().navigateUp()
+            viewLifecycleOwner.lifecycleScope.launch {
+                taskModel.insertTask(ModelTaskNew(title)).join()
+                findNavController().navigateUp()
+            }
         }
     }
 
